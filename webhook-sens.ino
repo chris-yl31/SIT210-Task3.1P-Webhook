d4b603d12a6c7f25f120f21d737cefd7e0815682
chris-yl31 <62497221+chris-yl31@users.noreply.github.com>
2020-04-14 17:24:24 +1000

    Add files via upload

---
+++ webhook-sens.ino
@@ -0,0 +1,30 @@
+int led1 = A5; 
+
+int led2 = D7; 
+
+int photoresistor = A0;
+int analogValue;
+
+
+void setup() {
+
+
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(photoresistor, INPUT);
+
+}
+
+
+void loop() {
+ 
+analogValue = analogRead(photoresistor);
+String light = String(analogValue);
+Particle.publish("light", light, PRIVATE);
+
+delay(30000);
+
+ 
+}
+
